52b49bff5c4266fba324ef2bdde53d4f871a4d75
Huthman King <kinghuthman@gmail.com>
2024-12-18 17:06:43 -0600

    update order of table creation

--- src/database/my_family.sql
+++ src/database/my_family.sql
@@ -10,7 +10,6 @@ CREATE TABLE users (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- connections table
 CREATE TABLE connections (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     user_id UUID NOT NULL REFERENCES users(id),
@@ -18,31 +17,6 @@ CREATE TABLE connections (
     relation_type VARCHAR(50) NOT NULL, -- e.g., "nuclear", "extended", "friend"
     connection_type VARCHAR(50) NOT NULL, -- e.g., "husband", "coworker", "neighbor"
     permissions JSONB DEFAULT '{}', -- Store custom visibility rules (optional)
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-
--- posts
-CREATE TABLE posts (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    dashboard_id UUID NOT NULL REFERENCES dashboards(id),
-    user_id UUID NOT NULL REFERENCES users(id),
-    type VARCHAR(50) NOT NULL, -- "text", "image", "video"
-    content TEXT, -- Text content for "text" posts
-    media JSONB, -- JSON array for "image" or "video" URLs
-    permissions JSONB DEFAULT '{}', 
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
--- comments table
-CREATE TABLE comments (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    parent_id UUID REFERENCES comments(id), -- For nested replies
-    post_id UUID NOT NULL REFERENCES posts(id),
-    planner_item_id UUID REFERENCES planner_items(id), -- Optional (if commenting on a task/event)
-    user_id UUID NOT NULL REFERENCES users(id),
-    content TEXT NOT NULL, -- Max 3000 characters
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
@@ -74,6 +48,29 @@ CREATE TABLE planner_items (
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+-- posts
+CREATE TABLE posts (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    dashboard_id UUID NOT NULL REFERENCES dashboards(id),
+    user_id UUID NOT NULL REFERENCES users(id),
+    type VARCHAR(50) NOT NULL, -- "text", "image", "video"
+    content TEXT, -- Text content for "text" posts
+    media JSONB, -- JSON array for "image" or "video" URLs
+    permissions JSONB DEFAULT '{}', 
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- comments table
+CREATE TABLE comments (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    parent_id UUID REFERENCES comments(id), -- For nested replies
+    post_id UUID NOT NULL REFERENCES posts(id),
+    planner_item_id UUID REFERENCES planner_items(id), -- Optional (if commenting on a task/event)
+    user_id UUID NOT NULL REFERENCES users(id),
+    content TEXT NOT NULL, -- Max 3000 characters
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
 -- Visibility Logic json (permissions column)
 {
